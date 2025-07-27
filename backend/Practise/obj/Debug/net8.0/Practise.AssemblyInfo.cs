2dc13456a6b6752925ce35354b3814d74c42e3d7
aurangzebhatti <164213606+abdull435@users.noreply.github.com>
2025-07-27 21:59:43 +0500

    second

--- backend/Practise/obj/Debug/net8.0/Practise.AssemblyInfo.cs
+++ backend/Practise/obj/Debug/net8.0/Practise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b810dacd96cf9cb2dd7f12bd88b9707e0afba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
