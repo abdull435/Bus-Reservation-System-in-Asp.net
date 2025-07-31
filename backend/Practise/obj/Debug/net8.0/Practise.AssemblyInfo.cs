ae356accd9bc3575d275310286a0458b058963d9
aurangzebhatti <164213606+abdull435@users.noreply.github.com>
2025-07-31 06:41:50 +0500

    change port of backend

--- backend/Practise/obj/Debug/net8.0/Practise.AssemblyInfo.cs
+++ backend/Practise/obj/Debug/net8.0/Practise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c751a57df9fce1f75d0eabcdcf581c4a0c249a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2243eeee958ce09f99d9cd7347a8aa0a8066bf37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
