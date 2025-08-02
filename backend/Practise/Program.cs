1dbbea246645c4c4de2ff3c1d104b11203246bd5
abdull435 <164213606+abdull435@users.noreply.github.com>
2025-08-02 09:42:43 +0500

    Update Program.cs

--- backend/Practise/Program.cs
+++ backend/Practise/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    // options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
     // options.Cookie.SameSite = SameSiteMode.None; 
 });
 builder.Services.AddEndpointsApiExplorer();
