23ff108e9f5f86cabcb194216876d6e241b188de
aurangzebhatti <164213606+abdull435@users.noreply.github.com>
2025-08-15 20:49:52 +0500

    make changes in ReservationDTO cinic data type from int o string and in db reservations table change

--- backend/Practise/DTO/ReservationDTO.cs
+++ backend/Practise/DTO/ReservationDTO.cs
@@ -5,7 +5,7 @@
         public int user_id { get; set; }
         public string name { get; set; }
         public string email { get; set; }
-        public int cinic { get; set; }
+        public string cinic { get; set; }
         public DateTime reservation_date { get; set; }
         public float total_price { get; set; }
         public int schedule_id { get; set; }
--- backend/Practise/obj/Debug/net8.0/Practise.AssemblyInfo.cs
+++ backend/Practise/obj/Debug/net8.0/Practise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a4b805e71abe1d03ef13b191ba0192d5643c7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19f9b7ccc4c0cfb63431b0d91d8ccf3c4d9b1496")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
